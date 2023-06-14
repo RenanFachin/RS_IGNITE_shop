60acde796a1da1fd4ca2d52f9d3191b95d6ce7ba
Renan <renan.fachin@hotmail.com>
2023-06-14 16:46:38 -0300

    Adiciona fontes externas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
+import {
+  Roboto_Flex as Roboto,
+} from 'next/font/google'
+
 import type { AppProps } from 'next/app'
 
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={roboto.className}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
