7c2d33f5b1557197a7b0587935bc305c6e6b78de
Renan <renan.fachin@hotmail.com>
2023-06-15 14:36:00 -0300

    Cria header default para a aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,24 @@ import {
 
 import type { AppProps } from 'next/app'
 import { globalStyles } from '@/styles/global'
-
+import Image from 'next/image'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 
 // Importando as estilizações globais e fazendo a sua inicialização
 globalStyles()
 
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '@/styles/pages/app'
+
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={roboto.className}>
+    <Container className={roboto.className}>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header> 
       <Component {...pageProps} />
-    </div>
+    </Container>
   )
 }
