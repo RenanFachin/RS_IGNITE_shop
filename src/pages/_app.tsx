fd5526113fee0d7047fe529f41ef44bd613380cf
Renan <renan.fachin@hotmail.com>
2023-06-15 14:22:11 -0300

    Cria objeto de estilizações globais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,13 @@ import {
 } from 'next/font/google'
 
 import type { AppProps } from 'next/app'
+import { globalStyles } from '@/styles/global'
 
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+
+// Importando as estilizações globais e fazendo a sua inicialização
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
