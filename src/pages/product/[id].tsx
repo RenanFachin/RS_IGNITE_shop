de99dac428bb34ccdea2b541e8af0279f8d1add8
Renan <renan.fachin@hotmail.com>
2023-06-19 14:27:57 -0300

    Cria layout da página de visualização dos produtos

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,27 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
+import Image from "next/image"
 import { useRouter } from "next/router"
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta x</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero modi eligendi nemo, odit debitis error ad sit ipsa dignissimos commodi tempore nulla nobis consequatur vero cum aspernatur odio aliquid in?</p>
+
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
