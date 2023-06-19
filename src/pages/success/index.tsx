a4e066f6c41ff3a5af4c5f59b9c347d3d47ef864
Renan <renan.fachin@hotmail.com>
2023-06-19 17:59:22 -0300

    Cria página de sucesso

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -1,7 +1,24 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
 
 export default function Success() {
 
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efeturada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhhuuu <strong>xxxxxxx</strong>, Sua camiseta <strong>xxxxxxx</strong> já está a caminhao da sua casa
+      </p>
+
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   )
 }
